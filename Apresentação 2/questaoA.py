53f31ea040a579a2319975bddb5d7f476f0eb706
ggmsbsb <guilhermem20g@gmail.com>
2024-04-23 19:30:57 -0300

    Add comentarios

--- Apresentação 2/questaoA.py
+++ Apresentação 2/questaoA.py
@@ -12,12 +12,12 @@ algodao = np.array([2015, 2357, 2319, 1577, 2576, 2835, 2700, 2853, 2851, 2537, 
 anos = anos.reshape(-1, 1)
 
 # Criando modelos de regressão linear para cada cultura
-modelo_milho = LinearRegression().fit(anos, milho)
+modelo_milho = LinearRegression().fit(anos, milho) #Cria uma instancia de regressão linear usando os parametros de anos e milho.
 modelo_soja = LinearRegression().fit(anos, soja)
 modelo_algodao = LinearRegression().fit(anos, algodao)
 
 # Fazendo previsões para o ano de 2024
-previsao_milho = modelo_milho.predict([[2024]])
+previsao_milho = modelo_milho.predict([[2024]]) #Executa o modelo de regressão linear, Y = AX + B e formulas de A e B que não consigo colocar aqui por causa dos caracteres.
 previsao_soja = modelo_soja.predict([[2024]])
 previsao_algodao = modelo_algodao.predict([[2024]])
 
@@ -42,10 +42,27 @@ print(f"Previsão para a produção de milho em 2024: {previsao_milho[0]:.2f} mi
 print(f"Limite Inferior para milho: {previsao_milho[0]-intervalo_milho:.2f} mil toneladas")
 print(f"Limite Superior para milho: {previsao_milho[0]+intervalo_milho:.2f} mil toneladas")
 
+print(" ")
+print("-------------------------------------------------------------------------------------")
+print(" ")
+
 print(f"Previsão para a produção de soja em 2024: {previsao_soja[0]:.2f} mil toneladas")
 print(f"Limite Inferior para soja: {previsao_soja[0]-intervalo_soja:.2f} mil toneladas")
 print(f"Limite Superior para soja: {previsao_soja[0]+intervalo_soja:.2f} mil toneladas")
 
+print(" ")
+print("-------------------------------------------------------------------------------------")
+print(" ")
+
 print(f"Previsão para a produção de algodão em 2024: {previsao_algodao[0]:.2f} mil toneladas")
 print(f"Limite Inferior para algodão: {previsao_algodao[0]-intervalo_algodao:.2f} mil toneladas")
 print(f"Limite Superior para algodão: {previsao_algodao[0]+intervalo_algodao:.2f} mil toneladas")
+
+print(" ")
+print("-------------------------------------------------------------------------------------")
+print(" ")
+
+#Valores de A, B e da equação de regressão linear. Só pra testar isso aq.
+#print(f"Para Milho, A = {modelo_milho.coef_[0]:.2f}, B = {modelo_milho.intercept_:.2f}, formula é Y = {modelo_milho.coef_[0]:.2f}X + {modelo_milho.intercept_:.2f}")
+#print(f"Para Soja, A = {modelo_soja.coef_[0]:.2f}, B = {modelo_soja.intercept_:.2f}, formula é Y = {modelo_soja.coef_[0]:.2f}X + {modelo_soja.intercept_:.2f}")
+#print(f"Para Algodao, A = {modelo_algodao.coef_[0]:.2f}, B = {modelo_algodao.intercept_:.2f}, formula é Y = {modelo_algodao.coef_[0]:.2f}X + {modelo_algodao.intercept_:.2f}")
