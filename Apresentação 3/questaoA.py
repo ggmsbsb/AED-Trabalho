f69dcbb882e58b4d3007c6796051cc61d09941d0
ggmsbsb <guilhermem20g@gmail.com>
2024-05-19 15:47:31 -0300

    tweaks

--- Apresentação 3/questaoA.py
+++ Apresentação 3/questaoA.py
@@ -29,7 +29,7 @@ p_val = t.sf(np.abs(t_stat), n-1) * 2
 
 # Imprimir os resultados
 print(f"O valor da estatística de teste é igual a {t_stat}")
-print(f"A confiança do teste é igual a {1 - p_val}")
+print(f"A confiança do teste é igual a {(1 - alpha) * 100}%") #1 - p_val
 print(f"O valor crítico da distribuição associada é igual a {t.ppf(1 - alpha, df=n - 1)}")
 if p_val < alpha:
     print("A hipótese/afirmação dada deve ser rejeitada")
