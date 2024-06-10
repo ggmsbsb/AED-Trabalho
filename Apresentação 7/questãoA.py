c3ab8532423a9a94bfef71858081c7bf61643ad9
ggmsbsb <guilhermem20g@gmail.com>
2024-06-10 19:05:56 -0300

    Fix warning ap7

--- Apresentação 7/questãoA.py
+++ Apresentação 7/questãoA.py
@@ -6,11 +6,8 @@ from sklearn.linear_model import LinearRegression
 # Dados fornecidos
 data = {
     'Ano': [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
-    'Índice Gini': [0,496300241, 0,519534818, 0,586416119, 0,601124934, 0,542588302, 0,494670229, 0,526021771, 0,591726337, 0,597650597, 0,535307008]
+    'Índice Gini': [0.496300241, 0.519534818, 0.586416119, 0.601124934, 0.542588302, 0.494670229, 0.526021771, 0.591726337, 0.597650597, 0.535307008]
 }
-
-# Corrigir valores decimais
-data['Índice Gini'] = [0.496300241, 0.519534818, 0.586416119, 0.601124934, 0.542588302, 0.494670229, 0.526021771, 0.591726337, 0.597650597, 0.535307008]
 
 # Criar DataFrame
 df = pd.DataFrame(data)
@@ -20,7 +17,6 @@ X = df[['Ano']]
 y = df['Índice Gini']
 
 # Criar e ajustar o modelo de regressão linear
-# Regressão Linear: y = a + bx
 model = LinearRegression()
 model.fit(X, y)
 
@@ -30,11 +26,10 @@ intercept = model.intercept_
 equacao_tendencia = f"Índice Gini = {coef:.6f} * Ano + {intercept:.6f}"
 
 # Prever o índice Gini para 2025
-ano_previsto = 2025
-gini_previsto = model.predict([[ano_previsto]])[0]
+ano_previsto = pd.DataFrame({'Ano': [2025]})
+gini_previsto = model.predict(ano_previsto)[0]
 
 # Calcular valores previstos e relativo cíclico
-#Rel Cicli = (Índice Gini / Gini Previsto) * 100
 df['Gini Previsto'] = model.predict(X)
 df['Relativo Cíclico (%)'] = 100 * df['Índice Gini'] / df['Gini Previsto']
 
